c098abc42f846cbd125d80e69cf20e1c7873602b
Vadim Medvedev <vadim130809@gmail.com>
2025-11-27 08:02:39 +0300

    feat: add merge-sort

--- algorithms/7_merge-sort.c
+++ algorithms/7_merge-sort.c
@@ -1,22 +1,23 @@
 /*
   Сортировка слиянием - уменьшает сложность по времени, но возрастает по памяти
   За счет выделения ячеек памяти под отсортированный массив - увеличивает сложность по памяти до линейной
-  Благодаря делению на левую и правые части (пока длина обоих не достигнет одного элемент) и сортировки их по отдельности, 
+  Благодаря делению на левую и правые части (пока длина обоих не достигнет одного элемент) и сортировки этих единичных элементов, 
   а затем слияние со сравнением левые и правые участи удается уменьшить временную сложность с квадратичной до линейно-логарифмической
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int merge_sort(int arr[], int len) {
-  // Base case for recursion
+int merge_sort(int* arr, float len) {
+  // Base case for recursion stops
   if (len <= 1) return 0;
   int mid = len / 2;
   
   //Prepare left and right arrays
-  int left_arr[] = malloc(mid * sizeof(int));
-  int right_arr[] = malloc((len - mid) * sizeof(mid));
+  int left_len = mid;
+  int right_len = len - mid; 
+  int* left_arr = malloc(left_len * sizeof(int));
+  int* right_arr = malloc(right_len * sizeof(int));
 
   //Fill left and right arrays with values of orginal one
   int right_i = 0;
@@ -32,14 +33,54 @@ int merge_sort(int arr[], int len) {
   }
 
   // Repeat algortihm for the left and right arrays until the base case
-  merge_sort(left_arr, sizeof(left_arr) / sizeof(int));
-  merge_sort(right_arr, sizeof(right_arr) / sizeof(int));
+  merge_sort(left_arr, left_len);
+  merge_sort(right_arr, right_len);
 
-  int left_len = sizeof(left_arr) / sizeof(int);
-  int right_len = sizeof(right_arr) / sizeof(int);
-  int sorted_arr = malloc(len * sizeof(int));
+  // On this stage recursion ends and we have left and right array than now we can merge in sorted order
+  int i = 0, l = 0, r = 0; //indicies for sorted array and left and right halves
 
+  // Repeat loop until left and right arrays are merged in right order
+  while (l < left_len && r < right_len) {
+    if (left_arr[l] < right_arr[r]) {
+      arr[i] = left_arr[l];
+      l++;
+    } else {
+      arr[i] = right_arr[r];
+      r++;
+    }
+    i++;
+  }
 
+  // Copy the remaining elements if there any
+  while (l < left_len) {
+    arr[i] = left_arr[l];
+    i++;
+    l++;
+  }
+
+  while (r < right_len) {
+    arr[i] = right_arr[r];
+    r++;
+    i++;
+  }
+  
+  free(left_arr);
+  free(right_arr);
 
   return 0;
+}
+
+int array[7] = {8, 4, 3, -5, 2, 9, 10};
+
+int main() {
+  unsigned int len = sizeof(array) / sizeof(int);
+  merge_sort(array, len);
+  for (int i = 0; i < len; i++) {
+    if (i + 1 == len) {
+      printf("%i", array[i]);
+    } else {
+      printf("%i, ", array[i]);
+    }
+  }
+  printf("\n");
 }
